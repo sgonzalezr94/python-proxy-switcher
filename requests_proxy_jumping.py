81b9b6824c0c6b769335ab5a91d256b876d49d77
Sebastian Gonzalez R <sebastiang1493@gmail.com>
2022-06-01 14:35:07 -0500

    Added 'proxy_job_execution' to accuratetly test the proxy switching.

--- requests_proxy_jumping.py
+++ requests_proxy_jumping.py
@@ -1,4 +1,5 @@
-from settings import IP_CHECKER_URL
+from proxy_switcher import ProxySwitcher
+from settings import IP_CHECKER_URL, PROXY_LIST, PORT_LIST, NUM_TOTAL_REQUESTS
 import json
 from json.decoder import JSONDecodeError
 import requests
@@ -16,3 +17,14 @@ def check_ip():
         )
     except JSONDecodeError:
         print("Request not completed. Failed JSON decoding.\n")
+
+
+def proxy_job_execution(requests_number: int = 10):
+    proxy_switcher = ProxySwitcher(PROXY_LIST, PORT_LIST)
+    for _ in range(requests_number):
+        proxy_switcher.set_proxy()
+        check_ip()  # This is meant to be changed to your scrapping job or task that requires proxy-switching
+
+
+if __name__ == "__main__":
+    proxy_job_execution(NUM_TOTAL_REQUESTS)
